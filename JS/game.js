a565d86b505cfcdf977fa6c4fb1b1851d54db434
robinetta <alxr19@gmail.com>
2022-10-11 16:35:45 -0400

    resizing function built

--- JS/game.js
+++ JS/game.js
@@ -20,6 +20,7 @@ function createGrid(){
 
 createGrid()
 
+
 const squares = document.querySelectorAll(".grid")
 
 squares.forEach((square) => {
@@ -28,8 +29,8 @@ squares.forEach((square) => {
     } )
 })
 
-
-const resetButton = document.querySelector("button")
+// Reset Button
+const resetButton = document.querySelector("#reset-btn")
 
 function resetGrid(){
     squares.forEach((square) => {
@@ -37,6 +38,32 @@ function resetGrid(){
         } )
 }
 
-
 resetButton.onclick = resetGrid
 
+// Change grid Size 
+
+const changeButton = document.querySelector("#size-btn")
+
+function UpdateGrid(){
+   //Get number from user
+    let newGridSizestyle = Number(prompt("What should the new grid size be?"))
+    newGridSize = newGridSizestyle * newGridSizestyle
+   // remove current grid
+    gridContainer.innerHTML = " "
+   //create new grid
+   for (i = 0; i < newGridSize; i++){
+    const div = document.createElement("div")
+    div.style.cssText = "border: 1px solid black;"
+    div.classList = "grid"
+    gridContainer.appendChild(div)
+}
+   // Update CSS Styling
+
+   gridContainer.style.gridTemplateColumns= ` repeat(${newGridSizestyle}, 2fr)`
+   gridContainer.style.gridTemplateRows= ` repeat(${newGridSizestyle}, 2fr)`
+
+
+   console.log(newGridSizestyle)
+}
+
+changeButton.onclick = UpdateGrid
